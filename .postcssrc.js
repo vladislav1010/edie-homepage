39137b82906db4b9f9eaf08862473191126a437a
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-09 08:36:05 +0300

    feat: add md solid primary button, migrate to postcss-cssnext; change typography; add rings and css vars
    Add primary color abstraction.

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-url"),
+    require("postcss-cssnext")({
+      features: {
+        customProperties: false,
+      },
+    }),
+  ],
+};
